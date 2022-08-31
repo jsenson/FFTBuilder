7ca0b58f78396de6c9a3b16f6ed435e7e137d97c
Jon Senson <jonsenson@gmail.com>
2022-08-31 00:54:24 -0400

    Update selected passive labels

--- Assets/Scripts/View/PassiveSlotView.cs
+++ Assets/Scripts/View/PassiveSlotView.cs
@@ -12,9 +12,15 @@ public class PassiveSlotView : MonoBehaviour {
 
 	public Ability.AbilityType PassiveType { get; private set; }
 
+	private CharacterBuild _character;
+	private bool _eventsSubscribed = false;
+
 	public void Refresh(CharacterBuild character, Ability.AbilityType type) {
+		UnsubscribeCharacterEvents();
+		_character = character;
 		PassiveType = type;
 		SetLabel(character.GetPassive(type), type);
+		SubscribeCharacterEvents();
 	}
 
 	public void Deselect() {
@@ -22,11 +28,27 @@ public class PassiveSlotView : MonoBehaviour {
 	}
 
 	private void OnEnable() {
+		SubscribeCharacterEvents();
 		_button.onClick.AddListener(OnSelectClicked);
 	}
 
 	private void OnDisable() {
+		UnsubscribeCharacterEvents();
 		_button.onClick.RemoveListener(OnSelectClicked);
+	}
+
+	private void SubscribeCharacterEvents() {
+		if (_character != null && !_eventsSubscribed) {
+			_character.OnPassiveChanged += OnPassiveSet;
+			_eventsSubscribed = true;
+		}
+	}
+
+	private void UnsubscribeCharacterEvents() {
+		if (_character != null && _eventsSubscribed) {
+			_character.OnPassiveChanged -= OnPassiveSet;
+			_eventsSubscribed = false;
+		}
 	}
 
 	private void SetLabel(Ability current, Ability.AbilityType type) {
@@ -38,6 +60,12 @@ public class PassiveSlotView : MonoBehaviour {
 		if (!_selection.Selected) {
 			_selection.Select();
 			OnSelected?.Invoke(this);
+		}
+	}
+
+	private void OnPassiveSet(CharacterBuild sender, Ability.AbilityType type) {
+		if (type == PassiveType) {
+			SetLabel(sender.GetPassive(type), type);
 		}
 	}
 }
